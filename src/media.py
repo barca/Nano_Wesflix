c49245fcce12388b1ee126b8d96ea2f88b3d2c7f
barca <tharden@wesleyan.edu>
2015-07-27 09:16:19 +0300

    incoperating instructor feedback

--- src/media.py
+++ src/media.py
@@ -1,6 +1,29 @@
 class movie:
 	def __init__(self, title, date, poster, desc, trailer, 
 		big_pic, director, runtime, main_cast_member, secondary_cast_member):
+		'''
+		The movie class encapsulates all the metadata 
+		associated with a particular film.  
+		All variables are treated as strings by the codebase.
+		EXAMPLE:
+		
+		the_philadelphia_story = movie(
+    		"The Philadelphia Story",
+    		"July 18",
+    		"http://bit.ly/1S3MYPB",
+    		"This 1940 Kathrine Hepburn, romantic comedy focuses on a young \
+    		soccalite and her relationship between her ex-husband, 
+    		fiance and a reporter. Unclear about her feelings for all \
+    		three men, Tracy must decide whom she truly loves.",
+			"https://www.youtube.com/embed/oCfuPPR7wnQ",
+    		"",
+    		"George Cukor",
+    		"112",
+    		"Kathrine Hepburn",
+    		"Cary Grant")
+
+
+		'''
 		self.title = title
 		self.date = date
 		self.poster = poster
--- src/run.py
+++ src/run.py
@@ -1,9 +1,13 @@
-from entertainment_center import *
-from detail_page import *
-from calendar_page import *
-from fresh_tomatoes import *
+from entertainment_center import Movies
+from detail_page import write_detailed_pages
+from calendar_page import write_calendar
+from WesFlix import write_and_open_homepage
+#Above imports necessary functions to generate the all files for the website
 
 
+#generates the calendar page, and links to the detailed pages, and main page
 write_calendar(Movies)
+#generates all the detailed pages for the movies
 write_detailed_pages(Movies)
-open_movies_page(Movies)
+#generates the homepage
+write_and_open_homepage(Movies)
